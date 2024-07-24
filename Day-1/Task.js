2fd7cd0d8e9fda8930482c269670ae3d75c82252
AdityaHeree <be.extrause@gmail.com>
2024-07-24 21:46:21 +0530

    Day 1

---
+++ Day-1/Task.js
@@ -0,0 +1,28 @@
+//Task-1
+    var value = 10;
+    console.log(value);
+
+//Task-2
+    let value1 = "Hello World...!!! ";
+    console.log(value1);
+
+//Task-3
+    const value2 = true;
+    console.log(value2);
+
+//Task-4
+    let obj = {
+        name : "Hemu",
+        age : 22
+    }
+
+    let arr = ["cat", "dog" , "cow"];
+    const cars = new Array("Maruti", "Volvo", "BMW");
+
+    console.log(typeof(value));
+    console.log(typeof(value1));
+    console.log(typeof(value2));
+    console.log(typeof(obj));
+    console.log(typeof(arr));
+    console.log(typeof(cars));
+    
